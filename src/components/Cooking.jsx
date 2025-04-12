8adf631f51cb797d723de15e1f6c37d103d1be59
vLimovs <limonytchannel@gmail.com>
2025-04-12 15:30:29 +0500

    asdfasd

--- src/components/Cooking.jsx
+++ src/components/Cooking.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import ordersStore from '../store/ordersStore';
 
 const Cooking = () => {
--- src/pages/Monitoring.jsx
+++ src/pages/Monitoring.jsx
@@ -1,10 +1,8 @@
-import React, { useEffect } from 'react'
-import ordersStore from '../store/ordersStore'
+import React from 'react'
 import Done from '../components/done';
 import Cooking from '../components/cooking';
 
 const Monitoring = () => {
-    const cooking = ordersStore((state) => state.cooking);
 
     return (
         <main className="main">
