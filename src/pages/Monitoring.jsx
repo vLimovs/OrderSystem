4683771930034cd8f975a7b43fa5cc0c3eb02942
vLimovs <limonytchannel@gmail.com>
2025-04-12 15:37:05 +0500

    admim

--- src/pages/Monitoring.jsx
+++ src/pages/Monitoring.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Done from '../components/Done';
 import Cooking from '../components/Cooking';
+import { Link } from 'react-router';
 
 const Monitoring = () => {
     return (
+        <>
         <main className="main">
             <Cooking />
             <Done />
         </main>
+        <Link to={'/admin'}>admin</Link>
+        </>
     );
 }
 
